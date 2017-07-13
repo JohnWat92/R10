b538119cd50a24e975d682c7db1769183749624b
Johnwat92 <JohnWat92@gmail.com>
2017-07-12 18:42:21 -0700

    clean code in speaker.js

--- js/scenes/Speaker/Speaker.js
+++ js/scenes/Speaker/Speaker.js
@@ -20,30 +20,27 @@ class Speaker extends Component {
   render() {
     const speakerData = this.props.speaker;
     const pictureSize = 100;
-    const aboutButton = () => {
 
-    }
     return (
       <View style={styles.container}>
-      <ScrollView>
-        <View style={styles.speakerBox}>
-          <Image
-            style={{width: pictureSize, height: pictureSize, borderRadius: pictureSize/2}}
-            source={{uri:`${speakerData.image}`}} />
-          <Text style={styles.speakerName}>{speakerData.name}</Text>
-          <Text style={styles.speakerDescription}>{speakerData.bio}</Text>
-          <LinearGradient
-            colors={[colors.purple, colors.blue]}
-            style={styles.aboutMoreButton}
-            start={{x: 1, y: 0}}
-            end={{x: -1, y: 1.0}}
-            locations={[0,.6]}
-          >
-          <Text style={styles.aboutMoreText}>Read More on Wikipedia</Text>
-          </LinearGradient>
-        </View>
-      </ScrollView>
-
+        <ScrollView>
+          <View style={styles.speakerBox}>
+            <Image
+              style={{width: pictureSize, height: pictureSize, borderRadius: pictureSize/2}}
+              source={{uri:`${speakerData.image}`}} />
+            <Text style={styles.speakerName}>{speakerData.name}</Text>
+            <Text style={styles.speakerDescription}>{speakerData.bio}</Text>
+            <LinearGradient
+              colors={[colors.purple, colors.blue]}
+              style={styles.aboutMoreButton}
+              start={{x: 1, y: 0}}
+              end={{x: -1, y: 1.0}}
+              locations={[0,.6]}
+            >
+            <Text style={styles.aboutMoreText}>Read More on Wikipedia</Text>
+            </LinearGradient>
+          </View>
+        </ScrollView>
       </View>
     );
   }
